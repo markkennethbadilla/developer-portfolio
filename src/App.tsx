a7684d3ed85665222cff7a272b67cb9194536a05
Mark <backupiammkb2002@gmail.com>
2025-12-30 14:30:40 +0800

    Add ScrollToTop component for improved navigation experience

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom'
+import { useEffect } from 'react'
 import HomePage from '@/components/pages/HomePage'
 import CaseStudyPage from '@/components/pages/CaseStudyPage'
 import ContactPage from '@/components/pages/ContactPage'
 import Navigation from '@/components/Navigation'
 import { Toaster } from '@/components/ui/sonner'
 
+function ScrollToTop() {
+  const { pathname } = useLocation()
+
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [pathname])
+
+  return null
+}
+
 function App() {
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <div className="min-h-screen">
         <Navigation />
         <main>
